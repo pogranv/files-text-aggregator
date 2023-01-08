c835ff9db16f12ee18eb2b802b241af2d4c76658
pogranv <andrei.rd18@gmail.com>
2023-01-08 12:15:35 +0300

    add main code

--- FilesTextAggregator/src/Main.java
+++ FilesTextAggregator/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
 public class Main {
+    /**
+     * Путь до корневой папки.
+     */
+    private final static String ROOTDIRECTORY = "root";
+
+    /**
+     * Код для склеивания содержимого файлов в корневой папке root с учетом зависимостей.
+     */
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var filesFinder = new FilesFinder(ROOTDIRECTORY);
+        var files = filesFinder.getFiles();
+        var filesDependeceBuilder = new FilesDependeceBuilder();
+        ArrayList<String> sortFiles;
+        try {
+            sortFiles = filesDependeceBuilder.getSortFilesList(files);
+        } catch (IncorrectFilesDependeceException e) {
+            System.out.println(e.getMessage());
+            for (var file : e.getNodesInCycle()) {
+                System.out.println(file);
+            }
+            return;
+        } catch (RequiredFileNotExistException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        var filesTextAggregator = new FilesTextAggregator();
+        System.out.println("====================================");
+        System.out.println("Список файлов с учетом зависимостей:");
+        filesTextAggregator.printFileNames(sortFiles);
+        System.out.println("====================================");
+        filesTextAggregator.printFilesText(sortFiles);
     }
 }
